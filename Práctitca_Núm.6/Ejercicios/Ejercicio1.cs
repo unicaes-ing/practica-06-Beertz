c5bbc8d3b70ba46262ed380deba6752ddac890a4
Alberto Galdámez <alberto.galdamez@catolica.edu.sv>
2019-09-05 09:31:10 -0600

    Enviar a Github

---
+++ Práctitca_Núm.6/Ejercicios/Ejercicio1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctitca_Núm._6
+{
+    class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
