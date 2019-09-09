ca9cbe2d499654d7c16c592b6f70bafc09085248
Alberto Galdámez <albeerto.galdamezh@gmail.com>
2019-09-09 12:49:07 -0600

    Ejercicio 4 pendiente de revision y detalles

---
+++ Práctica _Núm.6/Ejercicios/Ejercicio4.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Práctica__Núm._6.Ejercicios
+{
+    class Ejercicio4
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Escriba un numero decimal: ");
+            decimal enterNumero = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Su respuesta: ");
+            Console.Write(Cadena(enterNumero));
+            Console.ReadKey();
+        }
+
+        public static string Cadena(decimal numeroBase)
+        {
+            string cadenaCompletada = "";
+            for (int num = 0; num <= 2; num++)
+            {
+                if (numeroBase % 2 == 0)
+                {
+                    cadenaCompletada = cadenaCompletada + "0";
+
+                }
+                else
+                {
+                    cadenaCompletada = cadenaCompletada + "1";
+                }
+            }
+            return cadenaCompletada;
+        }
+    }
+}
