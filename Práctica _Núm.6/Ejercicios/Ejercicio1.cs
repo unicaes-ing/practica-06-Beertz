c0edaba79c9137a28d733b94060ed5732da39c7b
Alberto Galdámez <albeerto.galdamezh@gmail.com>
2019-09-09 11:21:50 -0600

    Ejercicio 1 terminado

--- Práctica _Núm.6/Ejercicios/Ejercicio1.cs
+++ Práctica _Núm.6/Ejercicios/Ejercicio1.cs
@@ -10,6 +10,69 @@ namespace Práctica__Núm._6
     {
         static void Main(string[] args)
         {
+            Console.Write("Escriba el numero de la tabla de multiplicar a generar: ");
+            int numTabla = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("---Color de Tabla----");
+            Console.WriteLine("[1] Rojo");
+            Console.WriteLine("[2] Azul");
+            Console.WriteLine("[3] Verde");
+            Console.WriteLine("[4] Amarillo");
+            Console.Write("Elija el color de la tabla (Presione solo [Enter] para elegir el color pre-establecido: ");
+            if (int.TryParse(Console.ReadLine(), out int numColor))
+            {
+                switch (numColor)
+                {
+                    case 1:
+
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        break;
+                    case 3:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case 4:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+
+
+
+
+                }
+
+                Console.Clear();
+                Console.WriteLine(DevolverTabla(numTabla));
+                Console.ReadKey();
+
+            }
+            else
+            {
+
+                Console.Clear();
+                Console.WriteLine(DevolverTabla(numTabla));
+                Console.ReadKey();
+
+            }
+
+
+        }
+
+        static string DevolverTabla(int numeroTabla)
+        {
+            string tablaFinal = "";
+            int num1 = 0;
+            int num2 = numeroTabla;
+            int final = 0;
+
+            for (int inicio = 1; inicio <= 10; inicio++)
+            {
+                num1 = num1 + 1;
+                final = num1 * num2;
+                Console.WriteLine("{0} * {1} = {2}", num2, num1, final);
+            }
+            return tablaFinal;
         }
     }
 }
